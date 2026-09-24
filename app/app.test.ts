e9b7a5cf11f3b1260346f1f37858874c5d197d44
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-25 02:48:06 +0300

    $bog_doodle_app: тест без локализованного заголовка

--- app/app.test.ts
+++ app/app.test.ts
@@ -2,9 +2,9 @@ namespace $ {
 
 	$mol_test({
 
-		'app has title'() {
+		'app shows intro in body'() {
 			const app = new $bog_doodle_app
-			$mol_assert_equal( app.title(), 'Doodle' )
+			$mol_assert_equal( app.body()[ 0 ], app.Intro() )
 		},
 
 	})
